0b64a76735a56d2046e777043be5a0873af00b67
Mateus.FCL <mateus.2021104787@unicap.br>
2024-09-11 13:08:22 -0300

    plano de fundo e imagem dos dados

--- app/src/main/java/com/example/jogodado/MainActivity.kt
+++ app/src/main/java/com/example/jogodado/MainActivity.kt
@@ -4,13 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jogodado.ui.theme.JogoDadoTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +36,75 @@ class MainActivity : ComponentActivity() {
         setContent {
             JogoDadoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(
+                                brush = Brush.linearGradient(
+                                    colors = listOf(
+                                        Color(0xFF099f17),
+                                        Color(0xFF024700)
+                                    ),
+                                    start = Offset(0f, 0f),
+                                    end = Offset(0f, Float.POSITIVE_INFINITY)
+                                )
+                            )
+                            .padding(innerPadding)
+                    ) {
+                        Column(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(5.dp),
+                            verticalArrangement = Arrangement.Center
+                        ) {
+                            Greeting(name = "Android")
+
+                            Spacer(modifier = Modifier.height(20.dp))
+
+                            Row(
+                                modifier = Modifier.fillMaxWidth(),
+                                horizontalArrangement = Arrangement.SpaceEvenly
+                            ) {
+
+                                Image(
+                                    painter = painterResource(id = R.drawable.dado_6),
+                                    contentDescription = "Jogo 1",
+                                    modifier = Modifier
+                                        .weight(1f)
+                                        .height(200.dp)
+                                )
+
+                                Spacer(modifier = Modifier.height(20.dp))
+
+                                Image(
+                                    painter = painterResource(id = R.drawable.dado_5),
+                                    contentDescription = "Jogo 2",
+                                    modifier = Modifier
+                                        .weight(1f)
+                                        .height(200.dp)
+                                )
+                            }
+
+                        }
+                    }
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    @Composable
+    fun Greeting(name: String, modifier: Modifier = Modifier) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    JogoDadoTheme {
-        Greeting("Android")
+    @Preview(showBackground = true)
+    @Composable
+    fun GreetingPreview() {
+        JogoDadoTheme {
+            Greeting("Android")
+        }
     }
 }
